f1ed64708ff839e18ec722e6f2d3344f811cd623
Andrew <andrewlikescheese@protonmail.com>
2025-07-22 23:41:26 +0300

    v0.2 Added looping when choices are made so user could try again, misc changes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"strings"
 )
 
 func main(){
@@ -18,26 +19,31 @@ func main(){
 		"^":true,
 		"sqrt":true,
 	}
-	
-	fmt.Println("calGolator v0.1")
+
+	fmt.Println("calGolator v0.2")
 	for {
+	for {
+		fmt.Println("Please choose your action. + - * / ^ sqrt")
+		fmt.Scan(&op)
+		op = strings.ToLower(strings.TrimSpace(op))
+		if !validOps[op] { // if op isn't in validOps
+			fmt.Println("Invalid operator:",op)
+		} else {
+			break
+		}
+	}
+
+	for {
+		fmt.Print("First number: ")
+		if _, err := fmt.Scan(&firstNum); err != nil {
+			fmt.Println("Invalid input for first number. Try again")
+		} else {
+			break
+		}
+	}
 	
-	fmt.Println("Please choose your action. + - * / ^ sqrt")
-	fmt.Scan(&op)
-
-	if !validOps[op] { // if op isn't in validOps
-		fmt.Println("Invalid operator:",op)
-		return
-	}
-
-	fmt.Print("First number: ")
-	if _, err := fmt.Scan(&firstNum); err != nil {
-		fmt.Println("Invalid input for first number")
-		return
-	}
-
 	if op == "sqrt"{ // we only need one number for sqrt
-		fmt.Printf("√%.2f = %.2f\n",firstNum,math.Sqrt(firstNum))
+		fmt.Printf("√%.2f = %.0f\n",firstNum,math.Sqrt(firstNum))
 		fmt.Println("Continue? y n")
 		fmt.Scan(&decision)
 		if decision == "n"{
@@ -45,38 +51,46 @@ func main(){
 		}
 
 	} else {
-		fmt.Print("Second number: ")
+	for {
+			fmt.Print("Second number: ")
 			if _, err := fmt.Scan(&secondNum); err != nil {
-				fmt.Println("Invalid input for second number")
-				return
-			}
-		
-			switch op {
-				case "+":
-					result = (firstNum + secondNum)
-				case "-":
-					result = (firstNum - secondNum)
-				case "*":
-					result = (firstNum * secondNum)
-				case "/":
-					if secondNum == 0 {
-						fmt.Println("Can't divide by zero")
-						return
-					}
-					result = (firstNum / secondNum)
-				case "^":
-					result = math.Pow(firstNum, secondNum)
-				default:
-					return
-			}
-			
-			fmt.Printf("%.2f %s %.2f = %.2f\n",firstNum, op, secondNum, result)
-			
-			fmt.Println("Continue? y n")
-			fmt.Scan(&decision)
-			if decision == "n"{
+				fmt.Println("Invalid input for second number. Try again")
+			} else {
 				break
 			}
+		}
+
+		switch op {
+			case "+":
+				result = (firstNum + secondNum)
+			case "-":
+				result = (firstNum - secondNum)
+			case "*":
+				result = (firstNum * secondNum)
+			case "/":
+				if secondNum == 0 {
+					fmt.Println("Can't divide by zero")
+					return
+				}
+				result = (firstNum / secondNum)
+			case "^":
+				result = math.Pow(firstNum, secondNum)
+			default:
+				return
+		}
+
+		if result == math.Trunc(result){
+			fmt.Printf("%.2f %s %.2f = %.0f\n",firstNum, op, secondNum, result)
+		} else {
+			fmt.Printf("%.2f %s %.2f = %.2f\n",firstNum, op, secondNum, result)
+		}
+
+		fmt.Println("Continue? y n")
+		fmt.Scan(&decision)
+		decision = strings.ToLower(strings.TrimSpace(decision))
+		if decision == "n"{
+			break
+		}
 	}
 	}
 	fmt.Println("Exiting...")		
