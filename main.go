4495fdb83de79eedc8c2548aa2f74997e8054d21
Andrew <andrewlikescheese@protonmail.com>
2025-07-22 17:04:13 +0300

    added ^ and sqrt

--- main.go
+++ main.go
@@ -1,23 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main(){
 	var answer string
 	var firstNum, secondNum float64
+	var result float64
 	var decision string
 
 	fmt.Println("calGolator ver.0.0.1")
 	for {
-		
 	
+	fmt.Println("Please choose your action. + - * / ^ sqrt")
 	
-	
-	fmt.Println("Please choose your action. + - * /")
-	
-	
-	fmt.Scanln(&answer)
+	fmt.Scan(&answer)
 	fmt.Println("You chose",answer)
+
 	
 	fmt.Print("First number: ")
 	if _, err := fmt.Scan(&firstNum); err != nil {
@@ -25,39 +26,48 @@ func main(){
 		return
 	}
 
-	fmt.Print("Second number: ")
-	if _, err := fmt.Scan(&secondNum); err != nil {
-		fmt.Println("Invalid input for second number.")
-		return
-	}
-
-	var result float64
-	switch answer {
-		case "+":
-			result = (firstNum + secondNum)
-		case "-":
-			result = (firstNum - secondNum)
-		case "*":
-			result = (firstNum * secondNum)
-		case "/":
-			if secondNum == 0 {
-				fmt.Println("division by 0 err")
+	if answer == "sqrt"{ // We only need one number for sqrt
+		fmt.Printf("√%.2f = %.2f\n",firstNum,math.Sqrt(firstNum))
+		fmt.Println("Continue? y n")
+		fmt.Scan(&decision)
+		if decision == "n"{
+			break
+		}
+	} else {
+		fmt.Print("Second number: ")
+			if _, err := fmt.Scan(&secondNum); err != nil {
+				fmt.Println("Invalid input for second number.")
 				return
 			}
-			result = (firstNum / secondNum)
-		default:
-			fmt.Println("operator err")
-			return
+		
+			switch answer {
+				case "+":
+					result = (firstNum + secondNum)
+				case "-":
+					result = (firstNum - secondNum)
+				case "*":
+					result = (firstNum * secondNum)
+				case "/":
+					if secondNum == 0 {
+						fmt.Println("division by 0 err")
+						return
+					}
+					result = (firstNum / secondNum)
+				case "^":
+					result = math.Pow(firstNum, secondNum)
+				default:
+					fmt.Println("operator err")
+					return
+			}
+			
+			fmt.Printf("%.2f %s %.2f = %.2f\n",firstNum, answer, secondNum, result)
+			
+			fmt.Println("Continue? y n")
+			fmt.Scan(&decision)
+			if decision == "n"{
+				break
+			}
 	}
-	
-	fmt.Printf("%.2f %s %.2f = %.2f\n",firstNum, answer, secondNum, result)
-	
-	fmt.Println("Continue? Y N")
-	fmt.Scanln(&decision)
-	if decision == "N"{
-		break
 	}
-	}
-	
 	fmt.Println("Exiting...")		
 }
