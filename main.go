fc60b9c28dc8756aaddec60778174ef6e508c7b6
Andrew <andrewlikescheese@protonmail.com>
2025-07-22 18:33:36 +0300

    added an actually good way to check for invalid operators,much more readable errors

--- main.go
+++ main.go
@@ -6,41 +6,52 @@ import (
 )
 
 func main(){
-	var answer string
+	var op string
 	var firstNum, secondNum float64
 	var result float64
 	var decision string
-
-	fmt.Println("calGolator ver.0.0.1")
+	validOps := map[string]bool{
+		"+":true,
+		"-":true,
+		"*":true,
+		"/":true,
+		"^":true,
+		"sqrt":true,
+	}
+	
+	fmt.Println("calGolator v0.1")
 	for {
 	
 	fmt.Println("Please choose your action. + - * / ^ sqrt")
-	
-	fmt.Scan(&answer)
-	fmt.Println("You chose",answer)
+	fmt.Scan(&op)
 
-	
-	fmt.Print("First number: ")
-	if _, err := fmt.Scan(&firstNum); err != nil {
-		fmt.Println("Invalid input for first number.")
+	if !validOps[op] { // if op isn't in validOps
+		fmt.Println("Invalid operator:",op)
 		return
 	}
 
-	if answer == "sqrt"{ // We only need one number for sqrt
+	fmt.Print("First number: ")
+	if _, err := fmt.Scan(&firstNum); err != nil {
+		fmt.Println("Invalid input for first number")
+		return
+	}
+
+	if op == "sqrt"{ // we only need one number for sqrt
 		fmt.Printf("√%.2f = %.2f\n",firstNum,math.Sqrt(firstNum))
 		fmt.Println("Continue? y n")
 		fmt.Scan(&decision)
 		if decision == "n"{
 			break
 		}
+
 	} else {
 		fmt.Print("Second number: ")
 			if _, err := fmt.Scan(&secondNum); err != nil {
-				fmt.Println("Invalid input for second number.")
+				fmt.Println("Invalid input for second number")
 				return
 			}
 		
-			switch answer {
+			switch op {
 				case "+":
 					result = (firstNum + secondNum)
 				case "-":
@@ -49,18 +60,17 @@ func main(){
 					result = (firstNum * secondNum)
 				case "/":
 					if secondNum == 0 {
-						fmt.Println("division by 0 err")
+						fmt.Println("Can't divide by zero")
 						return
 					}
 					result = (firstNum / secondNum)
 				case "^":
 					result = math.Pow(firstNum, secondNum)
 				default:
-					fmt.Println("operator err")
 					return
 			}
 			
-			fmt.Printf("%.2f %s %.2f = %.2f\n",firstNum, answer, secondNum, result)
+			fmt.Printf("%.2f %s %.2f = %.2f\n",firstNum, op, secondNum, result)
 			
 			fmt.Println("Continue? y n")
 			fmt.Scan(&decision)
